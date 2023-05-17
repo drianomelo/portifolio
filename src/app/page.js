41f88d904881f8f2f534cf5abc1f0b469b34a8a4
Adriano <adrianosenny@gmail.com>
2023-05-17 10:03:54 -0300

    Finalizando Header com ultima div de informações

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="{grobold.className} h-screen bg-background-dark bg-cover flex  items-center flex-col">
+    <div className="h-screen bg-background-dark bg-cover flex  items-center flex-col">
       <div>
         <img
           className="w-64 m-2"
@@ -8,7 +8,7 @@ export default function Home() {
           alt="logo Driano.css"
         ></img>
       </div>
-      <div className="w-3/5 h-3/4 bg-border-medium p-6 border-t-2 border-b border-x-2 border-border-light border-x-4 shadow-3xl relative bottom-7">
+      <div className="w-lg h-3/4 bg-border-medium p-6 border-t-2 border-b border-x-2 border-border-light border-x-4 shadow-3xl relative bottom-7">
         <div className="w-full h-full bg-cenario bg-cover px-6 py-4 border-t-8 border-l-4 border-r-4 border-border-dark">
           <header className="flex items-center justify-center relative">
             <div className="flex items-center gap-5 absolute left-0">
@@ -36,20 +36,27 @@ export default function Home() {
                 ></img>
               </button>
             </div>
-            <div className="text-center transition-transform hover:scale-105">
-              <h2 className="text-zinc-100 text-2xl mb-1 ">
-                driano.<span className="text-blue-400">css</span>
-              </h2>
-              <p className=" rounded-lg text-sm text-yellow-400 ">
-                Desenvolvedor Front-end
-              </p>
+            <div className="text-center cursor-pointer transition-transform hover:scale-105">
+              <img className="w-56" src="/img/title.svg"></img>
             </div>
-            <div className="absolute right-0">
-              <img
-                className="w-10 transition-all hover:scale-110"
-                src="/img/portifolio.png"
-                alt="prancheta"
-              ></img>
+            <div className="flex gap-7 absolute right-0">
+              <button>
+                <img
+                  className="w-10 transition-all hover:scale-110"
+                  src="/img/portifolio.png"
+                  alt="prancheta"
+                ></img>
+              </button>
+              <div className="flex flex-col gap-3 mt-5">
+                <div className="w-44 h-8 pt-1 pl-6 bg-clr-bege-light border-3 border-clr-bege text-base text-clr-black flex items-center relative">
+                  <img className="w-10 absolute -left-5 -top-2" src="/img/moeda.png"></img>
+                  Adriano Melo
+                </div>
+                <div className="w-44 h-8 pt-1 pl-6 bg-clr-bege-light border-3 border-clr-bege text-base text-clr-black flex items-center relative">
+                  <img className="w-10 absolute -left-5 -top-2" src="/img/donut.png"></img>
+                  21 anos
+                </div>
+              </div>
             </div>
           </header>
           <div>
