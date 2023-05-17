11679d7512cf9f38df539bbbe1ec5136293f8af7
Adriano Melo <adrianosenny@gmail.com>
2023-05-16 23:38:03 -0300

    Criando novos atributos para as classes do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "background-dark": "url('/img/background_dark.jpg')",
+        cenario: "url('/img/cenario.jpg')",
+      },
+      borderWidth: {
+        3: "3px",
+        6: "6px",
+      },
+      colors: {
+        "border-light": "#383838",
+        "border-medium": "#262626",
+        "border-dark": "#131717",
+        "clr-bege": "#B69E73",
+        "clr-bege-light": "#C1B197",
+        "clr-bege-medium": "#988566",
+        "clr-bege-dark": "#6A563E",
+        "clr-purple": "#8E00CC",
+        "clr-purple-dark": "#67158A",
+        "clr-yellow": "#C08A00",
+      },
+      boxShadow: {
+        "2xl": "4px 4px 0px ",
+        "3xl": "6px 10px 0px 0px rgba(0, 0, 0, 0.9)",
       },
     },
   },
   plugins: [],
-}
+};
